0e75981b6361c53588ab414a2e328855bb322da8
ingfrf <ingfrf@gmail.com>
2023-11-22 22:20:24 +0100

    Passwords hashed

--- src/main/java/com/enmivida/eazybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/enmivida/eazybank/config/ProjectSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -61,10 +61,15 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return new JdbcUserDetailsManager(dataSource);
-    }*/
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/enmivida/eazybank/controller/LoginController.java
+++ src/main/java/com/enmivida/eazybank/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.enmivida.eazybank.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final CustomerRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
+
             Customer savedCustomer = repository.save(customer);
             if (savedCustomer.getId() > 0) {
                 return ResponseEntity
